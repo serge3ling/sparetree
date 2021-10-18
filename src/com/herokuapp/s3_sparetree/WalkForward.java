007b610ef6356d6e2277b024e37d1d1127a1c627
sergio3 <serge3ling@gmail.com>
2021-10-18 21:38:55 +0300

    Add iterator

--- src/com/herokuapp/s3_sparetree/WalkForward.java
+++ src/com/herokuapp/s3_sparetree/WalkForward.java
@@ -1,10 +1,19 @@
 package com.herokuapp.s3_sparetree;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
 public class WalkForward {
+  private Iterator srcIterator;
+  private Iterator tgtIterator;
+
   public void walk(Set<Knot> srcKnots, Set<Knot> tgtKnots, List<Task> tasks) {
-    ;
+    srcIterator = srcKnots.iterator();
+    tgtIterator = tgtKnots.iterator();
+
+    while (srcIterator.hasNext()) {
+      Knot srcKnot = (Knot) srcIterator.next();
+    }
   }
 }
